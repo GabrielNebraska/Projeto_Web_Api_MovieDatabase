7e5a1553c8562afddd535be3de05929ad1f9c1bb
sentido1337 <abusentidoramaldavida@gmail.com>
2023-11-27 11:16:13 -0300

    TMDB

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -8,15 +8,40 @@ onMounted(async () => {
   const response = await api.get('genre/tv/list?language=pt-BR')
   genres.value = response.data.genres
 })
+
+const TV = ref([]);
+
+const listTV = async (genreId) => {
+    const response = await api.get('discover/tv', {
+        params: {
+            with_genres: genreId,
+            language: 'pt-BR'
+        }
+    });
+    TV.value = response.data.results
+};
 </script>
 
 <template>
   <h1>Programas de TV</h1>
   <ul class="genre-list">
-    <li v-for="genre in genres" :key="genre.id" class="genre-item">
-        {{ genre.name }}
-    </li>
+    <li v-for="genre in genres" :key="genre.id" @click="listTV(with_genres)" class="genre-item">
+    {{ genre.name }}
+</li>
   </ul>
+  <div class="tv-list">
+  <div v-for="tv in TV" :key="tv.id" class="tv-card">
+    
+    <img :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`" :alt="tv.name" />
+    <div class="tv-details">
+      <p class="tv-title">{{ tv.name }}</p>
+      <p class="tv-release-date">{{ tv.first_air_date }}</p>
+      <p class="tv-genres">{{ tv.with_genres }}</p>
+    </div>
+    
+  </div>
+</div>
+
 </template>
 
 <style scoped>
@@ -26,8 +51,9 @@ onMounted(async () => {
   flex-wrap: wrap;
   gap: 2rem;
   list-style: none;
-  padding: 0;
+  margin-bottom: 2rem;
 }
+
 
 .genre-item {
   background-color: #5d6424;
@@ -43,5 +69,37 @@ onMounted(async () => {
   cursor: pointer;
   background-color: #7d8a2e;
   box-shadow: 0 0 0.5rem #5d6424;
+}
+
+.tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tv-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
 }
 </style>
